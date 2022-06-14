61cbe15c56a93b260c6ea65bd2fcc8d18ab86a7c
Sam <noci0001@hz.nl>
2022-06-15 00:33:02 +0200

    great work

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {MdOutlineRestaurantMenu} from 'react-icons/md';
 import { SubHeading } from "../../components";
 
 // import { images } from '../../constants'; 
@@ -11,19 +11,8 @@ import './Header.css';
 const Header = () => (
   <div>
   <div className="app__header app__wrapper_for_main section__padding id='home'">
-    {/* <div className="app_wrapper_info">
-        <SubHeading
-          title="Chase the new flavor"/>
-          <h1 className='app__header-h1'> The Key to Fine Dining</h1>
-          <p className='p__opensans' style={{margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-          <button className='custom__button'> Explore the Menu</button>
-    </div>
-
-    <div className='app__wrapper_img'>
-        <img src={images.welcome} alt="header img"/>
-    </div> */}
-    <img src={images.finalLogo} alt="header img"/>
-    <button className='custom__button_special'> Explore the Menu</button>
+    <img src={images.finalLogo} alt="Johnsons Logo"/>
+    <button className='custom__button_special'><MdOutlineRestaurantMenu color="#000000" /> Explore the Menu </button>
   </div>
     
     </div>
